3ed656052844406206a8e591aaaa375c0152bf75
AntoniaGranit <antonia.granit@gmail.com>
2023-09-20 21:18:29 +0300

    finish menu component

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -13,78 +13,86 @@ import {
 } from "@heroicons/react/24/outline";
 
 const MenuItem = () => {
-  const MainLinks = [
-    {
-      id: 1,
-      icon: Square2StackIcon,
-      title: "Templates",
-    },
-    {
-      id: 2,
-      icon: ArrowDownTrayIcon,
-      title: "Import",
-    },
-    {
-      id: 3,
-      icon: TrashIcon,
-      title: "Trash",
-    },
-  ];
+  const MainLinks = {
+    title: null,
+    items: [
+      {
+        id: 1,
+        icon: Square2StackIcon,
+        title: "Templates",
+      },
+      {
+        id: 2,
+        icon: ArrowDownTrayIcon,
+        title: "Import",
+      },
+      {
+        id: 3,
+        icon: TrashIcon,
+        title: "Trash",
+      },
+    ],
+  };
 
-  const WorkspaceLinks = [
-    {
-      id: 1,
-      icon: DocumentIcon,
-      title: "Notes",
-    },
-    {
-      id: 2,
-      icon: HashtagIcon,
-      title: "Tasks",
-    },
-    {
-      id: 3,
-      icon: MegaphoneIcon,
-      title: "Announcements",
-    },
-    {
-      id: 4,
-      icon: MusicalNoteIcon,
-      title: "Music",
-    },
-    {
-      id: 5,
-      icon: QuestionMarkCircleIcon,
-      title: "Questions",
-    },
-    {
-      id: 6,
-      icon: CogIcon,
-      title: "Dashboard",
-    },
-    {
-      id: 7,
-      icon: CodeBracketIcon,
-      title: "Development",
-    },
-    {
-      id: 8,
-      icon: CogIcon,
-      title: "Swift",
-    },
-  ];
+  const WorkspaceLinks = {
+    title: "Workspace",
+    items: [
+      {
+        id: 1,
+        icon: DocumentIcon,
+        title: "Notes",
+      },
+      {
+        id: 2,
+        icon: HashtagIcon,
+        title: "Tasks",
+      },
+      {
+        id: 3,
+        icon: MegaphoneIcon,
+        title: "Announcements",
+      },
+      {
+        id: 4,
+        icon: MusicalNoteIcon,
+        title: "Music",
+      },
+      {
+        id: 5,
+        icon: QuestionMarkCircleIcon,
+        title: "Questions",
+      },
+      {
+        id: 6,
+        icon: CogIcon,
+        title: "Dashboard",
+      },
+      {
+        id: 7,
+        icon: CodeBracketIcon,
+        title: "Development",
+      },
+      {
+        id: 8,
+        icon: CogIcon,
+        title: "Swift",
+      },
+    ],
+  };
 
   const Menus = [MainLinks, WorkspaceLinks];
 
   return Menus.map((menu, menuIndex) => (
     <div className="py-5 border-indigo-400" key={menuIndex}>
-      {menu.map((item, itemIndex) => {
+      {menu.title ? (
+        <p className="text-xs text-lightgrey pb-5">
+          {menu.title.toUpperCase()}
+        </p>
+      ) : null}
+      {menu.items.map((item, itemIndex) => {
         const Icon = item.icon;
         return (
-          <div
-            key={itemIndex}
-            className="flex h-auto gap-3 border-2 border-indigo-400"
-          >
+          <div key={itemIndex} className="flex h-9 gap-4">
             <Icon className="h-5 w-5 inline-block" />
             <p className="text-xs inline-block">{item.title}</p>
           </div>
@@ -92,35 +100,6 @@ const MenuItem = () => {
       })}
     </div>
   ));
-
-  //   return (
-  //     <div>
-  //       <ul>
-  //         {MainLinks.map((item) => {
-  //           const Icon = item.icon;
-  //           return (
-  //             <div className="flex h-auto gap-3 border-2 border-indigo-400">
-  //               <Icon className="h-5 w-5 inline-block" />
-  //               <p className="text-xs inline-block">{item.title}</p>
-  //             </div>
-  //           );
-  //         })}
-  //       </ul>
-  //       <div>
-  //         <ul>
-  //           {WorkspaceLinks.map((item) => {
-  //             const Icon = item.icon;
-  //             return (
-  //               <div className="flex h-auto gap-3 border-2 border-indigo-400">
-  //                 <Icon className="h-5 w-5 inline-block" />
-  //                 <p className="text-xs inline-block">{item.title}</p>
-  //               </div>
-  //             );
-  //           })}
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   );
 };
 
 export default MenuItem;
