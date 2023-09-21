e635e86cc9a4d2544e8d59bf5ecd1ace7502065a
AntoniaGranit <antonia.granit@gmail.com>
2023-09-21 13:00:18 +0300

    note cards finished

--- src/components/NotesListItem.js
+++ src/components/NotesListItem.js
@@ -1,11 +1,31 @@
+import data from "../data/Notes.json";
+
 const NotesListItem = () => {
   return (
-    <div className="bg-primary-orange flex flex-col justify-between p-5 rounded-2xl  w-11/12 h-28">
-      <p className="text-white text-xs font-semibold">
-        Write down your ideas 💡
-      </p>
-      <p className="text-white text-xs">#ideas #to-do</p>
-      <p className="text-white text-xs">1 min</p>
+    <div className="flex flex-col items-center justify-center pt-52 gap-3 overflow-y-auto">
+      {data.map((note, index) => (
+        <div
+          key={index}
+          className="bg-primary-orange cursor-pointer flex flex-col justify-between p-5 rounded-2xl w-11/12 min-h-[120px]"
+        >
+          <p className="text-white text-xs font-bold">{note.title}</p>
+          <div>
+            {note.hashtags.length > 0 && (
+              <p className="text-white text-xs">
+                {note.hashtags.map((hashtag, hashtagIndex) => (
+                  <span key={hashtagIndex}>{hashtag} </span>
+                ))}
+              </p>
+            )}
+            <p className="text-white text-xs">
+              {note.description.length > 50
+                ? note.description.slice(0, 42) + "..."
+                : note.description}
+            </p>
+          </div>
+          <p className="text-white text-xs">{note.added}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/sections/NotesList.js
+++ src/sections/NotesList.js
@@ -3,14 +3,12 @@ import NotesListItem from "../components/NotesListItem";
 
 const NotesList = () => {
   return (
-    <div className="flex flex-col h-full w-2/5 gap-7">
+    <div className="flex flex-col h-full w-2/5 gap-2">
       <div className="flex flex-col gap-7">
         <SearchBar />
         <h1 className="font-semibold text-2xl pl-7">Notes</h1>
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <NotesListItem />
-      </div>
+      <NotesListItem />
     </div>
   );
 };
